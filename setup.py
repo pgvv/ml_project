09736ec90e29b5a7c0f4e5cbd5df1434f457f179
pgvv <pgvvishal31@gmail.com>
2023-12-20 19:02:00 +0530

    Logging and Exception

--- setup.py
+++ setup.py
@@ -23,9 +23,4 @@ setup(                                                          #metadeta of the
     author_email= 'g.pagadala06@gmail.com',
     packages= find_packages(),
     install_requires= get_requirements('requirements.txt')      #installs required libraries for the project
-)                                                      
-
-
-
-
-    
+)
---
+++ src/exception.py
@@ -0,0 +1,19 @@
+#Custom Exception Handling
+import sys      #Module manipulates different parts of Python runtime environment
+
+def error_message_detail(error, error_detail: sys):     #fn for how error message will look like
+    _, _, exc_tb= error_detail.exc_info()               #gives all info on which file and which line the exception has occurred
+    file_name= exc_tb.tb_frame.f_code.co_filename
+    error_message= "Error occured in Python script [{0}] line number [{1}] error message [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
+
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message= error_message_detail(error_message, error_detail= error_detail)
+
+    def __str__(self):
+        return self.error_message
